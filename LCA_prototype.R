afc0688701ab671d08cd913a806844637e1ff0e3
Yikai Zhao <zdwzhaoyikai@gmail.com>
2019-05-16 21:50:11 -0500

    adding profile plots folder

---
+++ LCA_prototype.R
@@ -0,0 +1,80 @@
+install.packages("poLCA")
+library("poLCA")
+library("reshape2")
+
+
+### turn columns into factor type
+nonnegtive[,2:length(nonnegtive)] <- lapply(nonnegtive[,2:length(nonnegtive)], factor)
+colnames(nonnegtive) = c("country", "tax", "religion", "free_election", "state_aid",
+                         "civil_rights", "women")
+
+AUS = nonnegtive %>%
+  filter(country == "Australia") %>%
+  dplyr::select(c("tax", "religion", "free_election", "state_aid",
+                  "civil_rights", "women"))
+
+US = nonnegtive %>%
+  filter(country == "United States") %>%
+  dplyr::select(c("tax", "religion", "free_election", "state_aid",
+                  "civil_rights", "women"))
+
+SWEDEN = nonnegtive %>%
+  filter(country == "Sweden") %>%
+  dplyr::select(c("tax", "religion", "free_election", "state_aid",
+                  "civil_rights", "women"))
+
+f = with(US, cbind(tax, religion, free_election, state_aid, civil_rights, women)~1) 
+
+min_bic <- 100000
+for(i in 2:7){
+  lc <- poLCA(f, US, nclass=i, maxiter=3000, 
+              tol=1e-5, na.rm=FALSE,  
+              nrep=10, verbose=TRUE, calc.se=TRUE)
+  if(lc$bic < min_bic){
+    min_bic <- lc$bic
+    LCA_best_model<-lc
+  }
+}    	
+LCA_best_model
+probs = LCA_best_model$probs
+probs$women %*% 1:10
+
+profile_tb = data.frame(
+                        tax = replicate(2, NA),
+                        religion = replicate(2, NA),
+                        free_election = replicate(2, NA),
+                        state_aid = replicate(2, NA),
+                        civil_rights = replicate(2, NA),
+                        women = replicate(2, NA))
+
+for (i in 1:6) {
+  if (length(probs[[i]][1,]) < 10) {
+
+    probs[[i]] = cbind(probs[[i]], matrix(0, nrow = nrow(probs[[i]]), ncol = 10 - ncol(probs[[i]]))) 
+  } 
+  profile_tb[, i] = probs[[i]] %*% 1:10
+}
+
+
+rownames(profile_tb) = c("class1", "class2")
+profile_tb = rownames_to_column(profile_tb)
+colnames(profile_tb)[1] = "class"
+profile_long = reshape::melt(profile_tb, id.vars = "class")
+
+
+p = ggplot(profile_long, aes(x = variable, y = value, group = class, color = class)) +
+  geom_point(size = 2.25)+
+  geom_line(size = 1.25) +
+  labs(x = NULL, y = "Mean value of the response", main = "Profile plot") +
+  theme_bw(base_size = 14)
+
+library(plotly)
+
+ggplotly(p, tooltip = "all") %>%
+  layout(legend = list(orientation = "h", y = 1.2))
+
+
+######
+
+LCA_best_model$P
+
---
+++ basic_block.R
@@ -0,0 +1,34 @@
+library(tidyverse)
+dat <- readRDS("/Users/zhaoyikai/Desktop/Demo_MI_project/wv5.rds")
+
+country_code = readxl::read_xlsx("country_code_5.xlsx", col_names = F)
+colnames(country_code) = c("code", "country")
+country_vector <- setNames(country_code$country, country_code$code)
+dat$country = country_vector[as.character(dat$V2A)]
+
+
+polity = readxl::read_xls("polity_2017.xls")
+polity_6up = polity %>%
+  filter(year == 2017 & democ >= 6) %>%
+  select(country, year, democ)
+country_name = polity_6up$country # select the atomic column using $ rather than []
+
+
+country_6up = intersect(polity_6up$country,country_vector)
+country_6up_censored = country_6up[-c(3, 5, 17, 19, 25,26,27,28, 31)]
+
+
+
+demo_v5 = c("country", "V152", "V153", "V154", "V155", "V157", "V161") 
+# No V156
+
+nonnegtive = dat %>% 
+  dplyr::select(demo_v5) %>%
+  filter(country %in% country_6up_censored) %>%
+  filter_all(all_vars(. > 0)) 
+
+
+
+
+
+
