f0595449d1e9e47672389e9b2f0fe512fed259ed
Yikai Zhao <zdwzhaoyikai@gmail.com>
2020-04-03 16:13:47 -0500

    add the setdiff(

---
+++ wave5_revisited/setdiff_countries_waves.R
@@ -0,0 +1,31 @@
+wv5 =  c( "United States",       "Canada",  "Trinidad and Tobago",
+          "Mexico",              "Guatemala",           "Colombia",           
+          "Peru"   ,             "Brazil" ,             "Chile",              
+          "Argentina",           "Uruguay" ,            "United Kingdom",     
+          "Netherlands",         "France" ,             "Switzerland",       
+          "Spain" ,              "Poland",              "Hungary"  ,          
+          "Italy"   ,            "Serbia",              "Slovenia" ,          
+          "Cyprus"   ,           "Bulgaria",            "Moldova" ,           
+          "Romania",             "Georgia",             "Finland",            
+          "Sweden"  ,            "Norway",              "Mali",               
+          "Burkina Faso",        "Ghana",               "Zambia",             
+          "Iraq",                "Taiwan"   ,           "Japan",              
+          "India",               "Malaysia",            "Indonesia",          
+          "Australia",           "New Zealand")
+
+wv6 =  c( "United States",       "Haiti",               "Trinidad and Tobago",
+          "Mexico",              "Ecuador",             "Peru",               
+          "Brazil",              "Chile",               "Argentina",          
+          "Uruguay",             "Netherlands",         "Spain",              
+          "Germany",             "Poland"  ,            "Slovenia",           
+          "Cyprus",              "Romania" ,            "Estonia",            
+          "Georgia",             "Sweden",              "Ghana",              
+          "Nigeria" ,            "South Africa",        "Tunisia",            
+          "Iraq",                "Lebanon" ,            "Kyrgyzstan",         
+          "Taiwan",              "Japan" ,              "India",              
+          "Pakistan",            "Malaysia",            "Philippines",        
+          "Australia",           "New Zealand" )
+
+
+
+setdiff(wv5, wv6)
